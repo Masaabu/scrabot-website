4cfd2b0f42ef33ce73336922bf92461486b23b73
Masaabu_ <115585647+Masaabu@users.noreply.github.com>
2025-04-20 20:57:03 +0900

    Update invite url

--- src/app/[locale]/invite/page.tsx
+++ src/app/[locale]/invite/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 function page() {
-  redirect('https://discord.com/oauth2/authorize?client_id=1330992974556823662&permissions=268716048&integration_type=0&scope=bot')
+  redirect('https://discord.com/oauth2/authorize?client_id=1330992974556823662&permissions=1125900175296512&integration_type=0&scope=bot')
 }
 
 export default page
